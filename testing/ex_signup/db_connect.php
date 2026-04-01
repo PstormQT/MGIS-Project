3e0f24fa3ff7e1cb55f3683a476bbe4ffc24c308
PstormQT <131224632+PstormQT@users.noreply.github.com>
2026-04-01 16:03:29 -0400

    Reorganize folder

--- testing/ex_signup/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Database configuration
-$host = 'studb-2.scb.rit.edu';
-$db   = '2255_MGIS445_';
-$user = '';
-$pass = '';
---
+++ testing/fastgen.py
@@ -0,0 +1,7 @@
+
+
+
+for i in range(0,9):
+    for j in range(0,4):
+        for k in range(0,10):
+            print(f"0{i}0{j}0{k},1000")
