5bb4a1096bc2581e4b578d97127cfb52714112c3
PstormQT <131224632+PstormQT@users.noreply.github.com>
2026-04-02 23:56:19 -0400

    Add data generation to SQL database

--- testing/fastgen.py
+++ testing/fastgen.py
@@ -4,4 +4,6 @@
 for i in range(0,9):
     for j in range(0,4):
         for k in range(0,10):
-            print(f"0{i}0{j}0{k},1000")
+            ShirtID = i * 10000 + j * 100 + k
+
+            print(f"{ShirtID}, 1000")
